2f3f7a5a68258aa6fb0f1741407ef0293168af81
Thomas Roger <troger.solutec@tf1.fr>
2012-03-15 17:13:16 +0100

    second commit

---
+++ app/cache/dev/assetic/config/1/143535fe588153fce39faa052cd867fb.php
@@ -0,0 +1,5 @@
+<?php
+
+// DaliaMepBundle:Default:index.html.twig
+return array (
+);
---
+++ src/Dalia/MepBundle/DaliaMepBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dalia\MepBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DaliaMepBundle extends Bundle
+{
+}
