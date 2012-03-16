d1436a2d37bb4211f1ba702f8551750439f9df8c
Thomas Roger <troger.solutec@tf1.fr>
2012-03-16 12:09:50 +0100

    18th commit

--- application/views/desktop.php
+++ application/views/desktop.php
@@ -2,7 +2,7 @@
 <?php $this->load->view('layout');?>
 
 <div id="desktop">
-	<?php foreach($query->result() as $row) : ?>
+	<?php foreach($desktop->result() as $row) : ?>
 	    <?php echo $row->prenom; ?>
 	    <?php echo $row->nom; ?>
 	    <?php echo $row->position; ?>
