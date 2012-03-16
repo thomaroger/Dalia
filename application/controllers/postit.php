9e1ade0c380b9d19c27b043a6fd1378e91d8e8aa
Damien Buton <dbuton.omnilog@tf1.fr>
2012-03-16 16:02:28 +0100

    commit de 16h02 apres le drop, #0

--- application/controllers/postit.php
+++ application/controllers/postit.php
@@ -7,15 +7,15 @@ class Postit extends CI_Controller {
     }
 
 	public function add()
-	{   
+	{
 	    $data = array();
 	    $desktopId = (int) $this->uri->segment(3);
 	    $data['desktop'] = $this->modeldesktop->getDesktopById($desktopId);
 	    $this->load->view('add_postit', $data);
 	}
-	
+
 	public function insert(){
-	    
+
 	    $this->modelpostit->insertPostIt();
 	    redirect('desktop/index/'.$_POST['desktop_id']);
 	}
--- js/openSpace.js
+++ js/openSpace.js
@@ -4,22 +4,18 @@
         $desktop.droppable({
             activeClass: "ui-state-highlight",
             drop: function( event, ui ) {
-                sendImage( ui.draggable );
+                sendImage( ui.draggable, $(this) );
             }
         });      
-        
-        $desktop.click(function() {
-            alert("Handler for .click() called.");
-        });
-        
-        function sendImage( $item ) {
+
+        function sendImage($item, $desktop) {
             var postit_id = $('input[name="postit_id"]').val();
-            var desktop_id = $('input[name="desktop_id"]').val();
+            var desktop_id = $desktop.find('input').val();
             
             $.ajax({
                 type: "POST",
                 url: '/postit/update',
-                data: {desktop_id : desktop_id, postit_id : postit_id},
+                data: {desktop_id : desktop_id, id : postit_id},
                 success: function(data) {
                   alert('Post-it envoyé !!!');
                   $item.remove();
