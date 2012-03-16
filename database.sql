d8a60f9844a1afa04c248d1fc100abcbdeb7c4f0
Thomas Roger <troger.solutec@tf1.fr>
2012-03-16 11:52:49 +0100

    15th commit

--- database.sql
+++ database.sql
@@ -1,13 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.0-rc1
+-- version 3.4.8
 -- http://www.phpmyadmin.net
 --
--- Serveur: localhost
--- Généré le : Jeu 15 Mars 2012 à 21:58
--- Version du serveur: 5.1.37
--- Version de PHP: 5.3.6
+-- Client: localhost
+-- Généré le : Ven 16 Mars 2012 à 11:52
+-- Version du serveur: 5.0.91
+-- Version de PHP: 5.3.10
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données: `dalia`
@@ -16,48 +23,33 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `desktop`
---
-
-CREATE TABLE IF NOT EXISTS `desktop` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `prenom` varchar(255) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `position` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Contenu de la table `desktop`
---
-
-INSERT INTO `desktop` (`id`, `prenom`, `nom`, `position`) VALUES
-(1, 'Thomas', 'Roger', 'Developpeur'),
-(2, 'Damien', 'Buton', 'Developpeur');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `postit`
 --
 
 CREATE TABLE IF NOT EXISTS `postit` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `user_id` int(11) NOT NULL,
   `desktop_id` int(11) NOT NULL,
   `titre` varchar(255) NOT NULL,
   `texte` varchar(255) NOT NULL,
   `posX` int(11) NOT NULL,
-  `posY` int(11) NOT NULL,      
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`desktop_id`)
-                        REFERENCES desktop( id)
-                        ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `posY` int(11) NOT NULL,
+  `date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `statut` tinyint(1) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `desktop_id` (`desktop_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Contenu de la table `postit`
 --
 
+INSERT INTO `postit` (`id`, `user_id`, `desktop_id`, `titre`, `texte`, `posX`, `posY`, `date`, `statut`) VALUES
+(1, 0, 1, 'postit1', 'Ceci est un postIt', 0, 0, '0000-00-00 00:00:00', 0),
+(2, 0, 1, 'postit2', 'Ceci est un postIt', 0, 0, '0000-00-00 00:00:00', 0),
+(3, 0, 1, 'postit3', 'Ceci est un postIt', 0, 0, '2012-03-16 10:08:31', 0),
+(4, 0, 2, 'postit4', 'Ceci est un postIt', 0, 0, '2012-03-16 10:10:26', 0);
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
